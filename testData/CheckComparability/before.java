f69c73c6dd60f7cbc217708a4512936a732ef85f
Sergei Smolin <equocredite@gmail.com>
2020-10-29 02:14:57 +0300

    fix test

--- testData/CheckComparability/before.java
+++ testData/CheckComparability/before.java
@@ -1,19 +1,4 @@
 class MyClass implements Comparable<MyClass> {
-    @Override
-    public int compareTo(MyClass that) {
-        if (this.comparableObject == null && that.comparableObject == null) {
-            // pass
-        } else if (this.comparableObject == null && that.comparableObject != null) {
-            return -1;
-        } else if (this.comparableObject != null && that.comparableObject == null) {
-            return 1;
-        } else if (this.comparableObject.compareTo(that.comparableObject) != 0) {
-            return (this.comparableObject.compareTo(that.comparableObject) < 0 ? -1 : 1);
-        }
-
-        return 0;
-    }
-
     private static class NonComparableClass {}
     private static class ComparableClass implements Comparable<ComparableClass> {
         private final int x;
